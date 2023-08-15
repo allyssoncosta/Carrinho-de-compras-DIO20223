4cdae4b7092376f3031df479a16662f70e81a17d
allyssonzao <costaallysson5@gmail.com>
2023-08-15 12:06:56 -0300

    testes 3 - allysson

--- src/CarrinhoDeCompra/CarrinhoDeCompra.java
+++ src/CarrinhoDeCompra/CarrinhoDeCompra.java
@@ -1,40 +1,43 @@
 package CarrinhoDeCompra;
-import java.sql.SQLOutput;
+
 import java.util.ArrayList;
 import java.util.List;
-public class CarrinhoDeCompra{
+
+public class CarrinhoDeCompra {
     List<Item> itens;
 
-    public void  CarrinhoDeCompras(){
-
-       itens = new ArrayList();
+    public CarrinhoDeCompra() {
+        itens = new ArrayList<>();
     }
-public void adicionarItem(String name, int quantidade, double preco){
+
+    public void adicionarItem(String name, int quantidade, double preco) {
         Item novoItem = new Item(quantidade, preco, name);
         itens.add(novoItem);
-    System.out.println("Item adicionado: " + name);
-}
-public void removerItem(String name){
+        System.out.println("Item adicionado: " + name);
+    }
+
+    public void removerItem(String name) {
         List<Item> removerItem = new ArrayList<>();
-        for(Item t: itens){
-if(t.name.equalsIgnoreCase(name)){
- removerItem.add(t);
-    System.out.println("Item removido: " + name);
-}
+        for (Item t : itens) {
+            if (t.name.equalsIgnoreCase(name)) {
+                removerItem.add(t);
+                System.out.println("Item removido: " + name);
+            }
         }
-}
-public double CalcularValorTotal(){
+    }
+
+    public double CalcularValorTotal() {
         double total = 0;
-        for(Item e : itens){
+        for (Item e : itens) {
             total += e.preco * e.quantidade;
         }
-    return total;
-}
-public void exibirItens(){
-    System.out.println("itens no carrinho: ");
-    for(Item b : itens){
-        System.out.println("Nome: " + b.name + "| Preço: " + b.preco + "| Quantidade: " + b.quantidade);
+        return total;
     }
-}
 
+    public void exibirItens() {
+        System.out.println("itens no carrinho: ");
+        for (Item b : itens) {
+            System.out.println("Nome: " + b.name + "| Preço: " + b.preco + "| Quantidade: " + b.quantidade);
+        }
+    }
 }
